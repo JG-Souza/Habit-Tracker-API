852731351d324df216965516af6a491624eab65c
JG-Souza <joaogsouzafma@gmail.com>
2025-04-03 08:21:46 -0300

    CRUD api habit controller

---
+++ app/Http/Controllers/HabitController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Habit;
+use App\Models\HabitLog;
+use App\Http\Resources\HabitResource;
+use App\Http\Requests\StoreHabitRequest;
+use App\Http\Requests\UpdateHabitRequest;
+
+
+class HabitController extends Controller
+{
+
+    public function index()
+    {
+        return HabitResource::collection(
+            Habit::all()
+        );
+    }
+
+    public function show(Habit $habit)
+    {
+        return HabitResource::make($habit);
+    }
+
+
+    public function store(StoreHabitRequest $request)
+    {
+        $data = $request->only('title', 'uuid');
+
+        $habit = Habit::create(array_merge($data, ['user_id' => 1]));
+
+        return HabitResource::make($habit);
+    }
+
+    public function update(UpdateHabitRequest $request, Habit $habit)
+    {
+        $habit->update($request->validated());
+
+        return HabitResource::make($habit);
+    }
+
+    public function destroy(Habit $habit)
+    {
+        HabitLog::whereHabitId($habit->id)->delete();
+
+        $habit->delete();
+
+        return response()->noContent();
+    }
+}
